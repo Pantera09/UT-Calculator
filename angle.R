ce5c4ac8f0ba514c983e2a5b883f1bf50b038ac8
Douglas Frey <douglasfrey@msn.com>
2016-08-02 15:43:40 -0700

    in progress changes for a function call.

--- angle.R
+++ angle.R
@@ -1,17 +1,26 @@
-
-angle<-function(){
+# data table with trig functions of 3 shearwave angles.
   
    trig<-data.frame(angle = c("45", "60", "70"),
                     sin = c(sin(45), sin(60), sin(70)),
                     cos = c(cos(45), cos(60), cos(70)), 
                     tan = c(tan(45), tan(60), tan(70)))
-  
-}
-
+                    
 #angle  sin        cos       tan
 #45  0.8509035  0.5253220 1.6197752
 #60 -0.3048106 -0.9524130 0.3200404
 #70  0.7738907  0.6333192 1.2219599
+
+#trig functions for given angle.
+
+sin(45)
+cos(70)
+tan(60)
+
+
+  
+
+
+
  
  
  
